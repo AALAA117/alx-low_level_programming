0c95f6e2b836520e8d41a6ff476b7c8f1d78e692
aalaa <alaamglal@gmail.com>
2023-09-08 17:56:32 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible combinations of three different digits,
+ * print_comb4 - Prints all possible combinations of four digits,
  *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+    int digit1, digit2, digit3, digit4;
 
-	for (digit1 = '0'; digit1 < '8'; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 <= '9'; digit3++)
-			{
-                                if ((digit2 != digit1) != digit3)
-				{
-					putchar(digit1);
-					putchar(digit2);
-					putchar(digit3);
-				}
-				if (digit1 == '7' && digit2 == '8')
-				{
-					continue;
-				}
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+    for (digit1 = '0'; digit1 < '8'; digit1++)
+    {
+        for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+        {
+            for (digit3 = digit2 + 1; digit3 <= '9'; digit3++)
+            {
+                for (digit4 = digit3 + 1; digit4 <= '9'; digit4++)
+                {
+                    if ((digit2 != digit1) != (digit3 != digit2) != (digit4 != digit3))
+                    {
+                        putchar(digit1);
+                        putchar(digit2);
+                        putchar(digit3);
+                        putchar(digit4);
+                    }
 
-	putchar('\n');
+                    if (digit1 == '7' && digit2 == '8' && digit3 == '9')
+                    {
+                        continue;
+                    }
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
 
-	return (0);
+    putchar('\n');
+
+    return (0);
 }
