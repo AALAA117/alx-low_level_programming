e017320873b9c8e6c97513fd8c94880ee140477c
aalaa <alaamglal@gmail.com>
2023-09-08 17:42:30 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,22 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (digit1 = '0'; digit1 < '8'; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			for (digit3 = digit2 + 1; digit3 <= '9'; digit3++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+                                if((digit2 != digit1) != digit3)
+				{
+					putchar(digit1);
+					putchar(digit2);
+					putchar(digit3);
+				}
+				if (digit1 == '7' && digit2 == '8')
+				{
 					continue;
-				
+				}
 				putchar(',');
 				putchar(' ');
 			}
@@ -29,7 +32,6 @@ int main(void)
 	}
 
 	putchar('\n');
-
 
 	return (0);
 }
