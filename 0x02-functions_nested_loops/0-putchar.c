646bc8ef92e794c4c496632734c33f4a4ef22a76
aalaa <alaamglal@gmail.com>
2023-09-12 12:45:02 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - the function _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c[9]={'_','p','u','t','c','h','a','r','\0'};
+	int i=0;
+
+	for(i=0;i<=8;i++)
+	{
+	 _putchar(c[i]);
+	}
+
+	return (0);
+}
