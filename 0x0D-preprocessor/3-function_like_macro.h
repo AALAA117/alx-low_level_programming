8ac8c50db9e48cf7235e52cbd39bb1dfd27956a4
aalaa <alaamglal@gmail.com>
2023-10-10 09:44:01 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef HEADER3_FUNCTION_LIKE_MACRO_H
+#define HEADER3_FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif
