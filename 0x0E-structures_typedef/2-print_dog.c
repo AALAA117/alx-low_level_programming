042a0dcaf4f2f577147a94586bab4bc44b98b798
aalaa <alaamglal@gmail.com>
2023-10-10 15:37:57 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,9 +23,9 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name : %s\n", d->name);
-			printf("Age : %f\n", d->age);
-			printf("Owner : %s\n", d->owner);
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
