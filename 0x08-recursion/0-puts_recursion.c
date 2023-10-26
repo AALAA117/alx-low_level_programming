192ab22f4d9390b3c515dd6416794c16e134eaad
aalaa <alaamglal@gmail.com>
2023-10-26 09:13:09 -0700

    c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,15 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s == '\0')
+	if (*(s + i) == '\0')
 	{
 		return;
 	}
 	else
 	{
 		_putchar(s[i]);
-		s++;
-		_puts_recursion(s);
+		i = i + 1;
+		_puts_recursion(s + i);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
