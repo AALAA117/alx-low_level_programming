6c4321554cab44345949059ee8fa664b876917dd
aalaa <alaamglal@gmail.com>
2023-10-27 16:54:23 -0700

    c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@ int main(int __attribute__((unused)) argc, char *argv[])
 {
 	while (*argv[0] != '\0')
 	{
-		_putchar(*argv[0]);
-		*argv[0] = *(argv[0] + 1);
+		putchar(*argv[0]);
+		argv[0] = (argv[0] + 1);
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
