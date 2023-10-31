bfa338e286fa652d486103c530e88db8f734ae06
aalaa <alaamglal@gmail.com>
2023-10-30 23:02:53 -0700

    c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two strings.
+ * @s1: string.
+ * @s2: string.
+ * Return: string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int len = 0;
+	int len1 = 0;
+	int len2 = 0;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (" ");
+	}
+	while (*s2 != '\0' || *s1 != '\0')
+	{
+		len++;
+	}
+	ptr = malloc(len + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (*s1 != '\0')
+		{
+			ptr[len1] = s1[len1];
+			len1++;
+		}
+		while (*s2 != '\0')
+		{
+			ptr[len1] = s2[len2];
+			len2++;
+			len1++;
+		}
+	}
+	ptr[len1] = '\0';
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+
 #endif
