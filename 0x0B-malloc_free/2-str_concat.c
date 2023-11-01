518ee6488ca0229cdb77738f1ede2a5d8139f090
aalaa <alaamglal@gmail.com>
2023-11-01 04:37:37 -0700

    C

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 	{
-		return (" ");
+		return ("");
 	}
-	while (*(s2 + len) != '\0' || *(s1 + len) != '\0')
+	while (s1[len] != '\0' || s2[len] != '\0')
 	{
 		len++;
 	}
+	len++;
 	ptr = malloc(len);
 	if (ptr == NULL)
 	{
