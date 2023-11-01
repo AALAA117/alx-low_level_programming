603755d9edd7ea48c7c16db6845316e3c6766a65
aalaa <alaamglal@gmail.com>
2023-11-01 13:00:49 -0700

    C

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,17 +15,10 @@ char *str_concat(char *s1, char *s2)
 
 	s1 = (s1 != NULL) ? s1 : "";
 	s2 = (s2 != NULL) ? s2 : "";
-	while (s1[len1] != '\0')
+	while (s1[len] != '\0' || s2[len] != '\0')
 	{
 		len++;
-		len1++;
 	}
-	while (s2[len2] != '\0')
-	{
-		len++;
-		len2++;
-	}
-	len--;
 	ptr = malloc(len * sizeof(char));
 	if (ptr == NULL)
 	{
@@ -33,14 +26,12 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
-		len1 = 0;
 		while (s1[len1] != '\0')
 		{
 			ptr[i] = s1[len1];
 			len1++;
 			i++;
 		}
-		len2 = 0;
 		while (s2[len2] != '\0')
 		{
 			ptr[i] = s2[len2];
