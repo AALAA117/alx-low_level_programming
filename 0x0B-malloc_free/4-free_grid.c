3e769fccfc9188fbaa46a28f7a94014124a30f4d
aalaa <alaamglal@gmail.com>
2023-11-01 12:30:48 -0700

    C

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,16 +9,9 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL)
-	{
-		return;
-	}
 	for (i = 0; i < height; i++)
 	{
-		if (grid[i] != NULL)
-		{
-			free(grid[i]);
-		}
+		free(grid[i]);
 	}
 	free(grid);
 }
