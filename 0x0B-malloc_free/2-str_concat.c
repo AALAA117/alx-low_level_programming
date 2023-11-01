9c85be86170e9bbfd546f71a6f9ab37cf38dc134
aalaa <alaamglal@gmail.com>
2023-11-01 07:14:07 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,8 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 	int i = 0;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		s1 = (s1 != NULL) ? s1 : "";
-		s2 = (s2 != NULL) ? s2 : "";
-	}
+	s1 = (s1 != NULL) ? s1 : "";
+	s2 = (s2 != NULL) ? s2 : "";
 	while (s1[len1] != '\0')
 	{
 		len++;
