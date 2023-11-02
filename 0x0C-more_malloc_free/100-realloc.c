95e084a3540414fe11857f876a4c3501742994ae
aalaa <alaamglal@gmail.com>
2023-11-02 16:56:51 -0700

    C

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+
+	ptr = malloc(old_size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (malloc(new_size));
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		if (new_size == old_size)
+		{
+			return (ptr);
+		}
+		else if (new_size > old_size)
+		{
+			ptr = malloc(new_size);
+			for (i = 0; i < (old_size / sizeof((void*)ptr)); i++)
+			{
+				return (ptr);
+			}
+			free(ptr);
+		}
+		else
+		{
+			i = 0;
+			while (i < (old_size / sizeof((void*)ptr)))
+			{
+				return (ptr);
+				i++;
+			}
+			free(ptr);
+		}
+	}
+	return (ptr);
+}
