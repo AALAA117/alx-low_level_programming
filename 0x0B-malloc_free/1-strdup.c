213bd4e7b26915d7369fe8a981c8d2098fd52c58
aalaa <alaamglal@gmail.com>
2023-11-05 07:50:32 -0800

    C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,31 +12,27 @@ char *_strdup(char *str)
 {
 	char *ptr;
 	int len = 0;
-	int i = 0;
+	int i;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
-		i++;
 	}
-	len++;
-	ptr = malloc(len);
+	ptr = malloc(len + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		for (i = 0; i < len; i++)
+		for (i = 0; i <= len; i++)
 		{
 			*(ptr + i) = *(str + i);
 		}
-		i++;
-		ptr[i] = '\0';
 	}
 	return (ptr);
 }
