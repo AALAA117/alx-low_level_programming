a8284a8cbbf4a08beef022e52203ab402d44b7d4
aalaa <alaamglal@gmail.com>
2023-11-06 13:09:37 -0800

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#define NIL (nil)
 #include <stdio.h>
 #include "dog.h"
 /**
@@ -9,12 +8,12 @@
  * @age:..
  * @owner:..
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
