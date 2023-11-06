61da5fc7b404e95429a45865908f89d17b8e1422
aalaa <alaamglal@gmail.com>
2023-11-06 14:43:34 -0800

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,18 +9,49 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *ptr;
+	struct dog *p;
+	struct dog *c;
+	int i, j;
 
 	ptr = malloc(sizeof(struct dog));
-	if (ptr == NULL)
+	if (ptr == NULL || p == NULL)
 	{
 		return (NULL);
 	}
-	else
+	i = 0;
+	while (name[i] != '\0')
 	{
-		ptr->name = name;
-		ptr->age = age;
-		ptr->owner = owner;
+		i++;
 	}
+	j = 0;
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc(sizeof(struct dog));
+	ptr->name = malloc((i + 1) * sizeof(char));
+	ptr->name = name;
+	_memcopy(&p, &ptr, (i + 1) * sizeof(char));
+	ptr->owner = malloc((j + 1) * sizeof(char));
+	ptr->owner = owner;
+	_memcopy(&c, &ptr, (j + 1) * sizeof(char));
 	return (ptr);
 }
-
+/**
+ * _memcopy - copy
+ * @dst:..
+ * @src:..
+ * @n:..
+ *
+ * Return: nothing
+ */
+void *_memcopy(void *dst, void *src, size_t n)
+{
+	k = 0;
+	while (k < n)
+	{
+		dst[k] = src[k];
+		k++;
+	}
+	return (dst);
+}
