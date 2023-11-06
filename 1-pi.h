b68cc5fc682d4716a64bfbb1783712f69b4a0876
aalaa <alaamglal@gmail.com>
2023-11-06 04:06:53 -0800

    c

---
+++ 1-pi.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_1_PI_H
+#define HEADER_1_PI_H
+#include <stdio.h>
+#define PI 3.14159265359
+#endif
---
+++ 3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_3_FUNCTION_LIKE_MACRO_H
+#define HEADER_3_FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#endif
