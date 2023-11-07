22bc33f187b474a51ef7f4ddd9f650342c1f338b
aalaa <alaamglal@gmail.com>
2023-11-07 07:41:02 -0800

    C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,31 +12,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog *ptr;
 	int n_len, o_len;
 
-	ptr = malloc(1 + sizeof(struct dog));
+	ptr = malloc(sizeof(struct dog));
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	n_len = strlen(name);
 	ptr->name = malloc((n_len + 1) * sizeof(char));
 	if (ptr->name == NULL)
 	{
-		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->name, name);
+	ptr->name = strcpy(ptr->name, name);
 	o_len = strlen(owner);
 	ptr->owner = malloc((o_len + 1) * sizeof(char));
 	if (ptr->owner == NULL)
 	{
 		free(ptr->name);
-		free(ptr->owner);
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->owner, owner);
+	ptr->owner = strcpy(ptr->owner, owner);
 	ptr->age = age;
 	return (ptr);
 }
