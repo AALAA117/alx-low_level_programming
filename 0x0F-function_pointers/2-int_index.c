54c69d841e0fb60484a65cedabed8cca765ab733
aalaa <alaamglal@gmail.com>
2023-11-08 06:21:21 -0800

    C

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array:..
+ * @size:..
+ * @cmp:..
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 98 || cmp(array[i]) == -98)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
