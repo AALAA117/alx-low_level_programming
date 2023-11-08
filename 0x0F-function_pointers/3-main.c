969d00922f20f75b3a31416cec21ed494c0f273f
aalaa <alaamglal@gmail.com>
2023-11-08 14:45:02 -0800

    C

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2] == NULL)
+	{
+		get_op_func(NULL);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	switch (*argv[2])
