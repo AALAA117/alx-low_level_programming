e4577eb5de68f36177a5d6408928e6b74e530dcc
aalaa <alaa>
2023-11-10 15:08:07 -0800

    C

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i = 0;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(args, n);
 	while (i < n)
 	{
