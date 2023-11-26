97d59f548dea3e5c81cd476230aaab5e3889db34
aalaa <alaamglal@gmail.com>
2023-11-26 15:09:26 -0800

    c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,5 +6,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
+	if (n == 0)
+		_putchar('0');
+	while ((n & mask) == 0 && mask != 0)
+	{
+		mask = mask >> 1;
+	}
+	while (mask != 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask = mask >> 1;
+	}
+}
