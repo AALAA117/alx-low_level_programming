e8d71bdb358738221cc4ed6affcf0bf556e99926
aalaa <alaamglal@gmail.com>
2023-11-29 15:54:30 -0800

    c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it standard output.
+ * @filename: name of file
+ * @letters:..
+ * Return: number of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd, fd2;
+	int n;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		exit(1);
+	}
+	fd2 = open(filename, O_RDWR);
+	if (fd2 < 0)
+	{
+		return (0);
+	}
+	fd = read(fd2, buf, letters);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	n = write(STDOUT_FILENO, buf, letters);
+	if (n < 0)
+	{
+		return (0);
+	}
+	return (letters);
+}
