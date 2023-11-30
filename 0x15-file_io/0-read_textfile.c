d228bbc1a2f7c1876ef7f6349f214ddce74a014e
aalaa <alaamglal@gmail.com>
2023-11-29 16:20:18 -0800

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	int fd, fd2;
+	int n_bytes, fd2;
 	int n;
 
 	if (filename == NULL)
@@ -20,14 +20,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		exit(1);
 	}
-	fd2 = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	fd2 = open(filename, O_RDONLY | O_CREAT, S_IRUSR);
 	if (fd2 == -1)
 	{
 		return (0);
 	}
-	fd = read(fd2, buf, letters);
-	if (fd < 0)
+	n_bytes = read(fd2, buf, letters);
+	if (n_bytes < 0)
 	{
+		close(fd2);
 		return (0);
 	}
 	n = write(STDOUT_FILENO, buf, letters);
@@ -35,5 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+	close(fd2);
+	free(buf);
 	return (letters);
 }
