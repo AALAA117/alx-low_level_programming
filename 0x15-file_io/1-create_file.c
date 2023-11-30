d6b63b388e1023a880ff37f322c04249c380e258
alaa <alaamglal@gmail.com>
2023-11-30 11:49:19 -0800

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,44 +7,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, len = 0;
-	char *buf;
-	ssize_t n;
+	int fd, i = 0;
+	ssize_t n = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		fd = creat(filename, S_IRUSR | S_IWUSR);
-		exit(0);
 	}
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		exit(0);
+	}
 	while (text_content[i] != '\0')
 	{
-		len++;
 		i++;
 	}
-	buf = malloc(sizeof(char) * (len + 1));
-	i = 0;
-	while (text_content[i] != '\0')
-	{
-		buf[i] = text_content[i];
-		i++;
-	}
-	buf[i] = '\0';
-	n = write(fd, buf, (len + 1));
+	n = write(fd, text_content, i);
 	if (n == -1)
 	{
-		free(buf);
+		close(fd);
 		return (-1);
 	}
 	close(fd);
-	free(buf);
 	return (1);
 }
