bc544699026eed78a5d3018b6995ad5d91e63a58
alaa <alaamglal@gmail.com>
2023-12-01 14:48:25 -0800

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content == NULL)
 	{
-		exit(0);
+		text_content = "";
 	}
 	while (text_content[i] != '\0')
 	{
