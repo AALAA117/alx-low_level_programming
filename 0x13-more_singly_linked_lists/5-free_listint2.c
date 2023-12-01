815fb2b9424090a16bf65d4f66af364966c7e305
alaa <alaamglal@gmail.com>
2023-12-01 13:45:25 -0800

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 	{
-		head = &current;
+		return;
 	}
 	while (*head != NULL)
 	{
@@ -19,5 +19,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = current;
 	}
-	*head = NULL;
 }
