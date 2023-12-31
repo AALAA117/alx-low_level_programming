372b2173b11e2af7ec8fbcb91e2caab1f447beb0
alaa <alaamglal@gmail.com>
2023-12-31 09:07:34 -0800

    done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * _abs - calculate the absolute value of the number from zero.
- * @i: the integer to be converted.
- * Return: absolute value of the input number.
+ * _abs - computes the absolute value of an integer.
+ * @n: number
+ * Return: integer
  */
-int _abs(int i)
+int _abs(int n)
 {
-	if (i < 0)
-		i = i * -1;
-	return (i);
+	if (n < 0)
+	{
+		n = -(n);
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
 }
