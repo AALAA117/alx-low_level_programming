20eeb772e447b0120e0c70e7b2189d7e2053a28f
alaa <alaamglal@gmail.com>
2023-12-31 15:10:36 -0800

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,27 +16,25 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			mul = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + mul);
+			}
 			if (mul >= 10)
 			{
 				first_dig = mul / 10;
 				last_dig = mul % 10;
+				_putchar(',');
+				_putchar(32);
 				_putchar(first_dig + '0');
 				_putchar(last_dig + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
-				_putchar(32);
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
 				_putchar(mul + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
-				_putchar(32);
-				_putchar(32);
 			}
 		}
 		_putchar('\n');
