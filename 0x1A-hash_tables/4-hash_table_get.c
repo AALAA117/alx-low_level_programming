3adcb69abed51e94a3b7016acb5b0091d4aa8c6d
alaa <alaamglal@gmail.com>
2024-02-07 06:33:20 -0800

    done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,24 @@
 #include "hash_tables.h"
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_get - get value of specific key
+ * @ht:pointer to table
+ * @key:..
+ * Return: string
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *current;
 
+	index = key_index((void *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return ("(null)");
 }
